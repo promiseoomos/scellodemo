a7fa2060f1a96f3991395ee3db31f5d15699407d
Promise Omos <promzybankz@gmail.com>
2022-07-20 00:31:07 +0100

    95% Completion rate achieved

--- pages/index.vue
+++ pages/index.vue
@@ -18,9 +18,15 @@
 
                 <div class="flex flex-wrap justify-between p-3">
                     <div class="w-1/2">
-                        <button class="border border-[#C6C2DE] p-3 text-xl mr-4 rounded-lg w-24 text-[#25213B]"><img src="~/assets/filter.png" class="inline-block"/> Filter</button>
+                        <button @click="showfilters = !showfilters" class="border border-[#C6C2DE] p-3 text-xl mr-4 rounded-lg w-24 text-[#25213B]"><img src="~/assets/filter.png" class="inline-block"/> Filter</button>
                         <input @keyup="searcher()" v-model="searchtext" class="p-3 pl-12 w-4/5 bg-gray-100 outline-0 focus:outline-0 hover:border hover:border-[#6D5BD0] rounded-lg" placeholder="Search User by Name, Email or Date">
                         <img src="~/assets/search.png" class="relative -top-8 left-32 z-50">
+                    </div>
+                    <div v-if="false" class="bg-white rounded-lg  w-56 absolute left-10 top-1/3 z-50">
+                        <button class=" hover:bg-gray-200 w-full p-2 block text-sm text-left">Edit</button>
+                        <button class=" hover:bg-gray-200 w-full p-2 block text-sm text-left">View Profile</button>
+                        <button class="text-green-600 hover:text-green-400 hover:bg-gray-200 p-2 block text-sm w-full text-left">Activate user</button>
+                        <button class="text-red-600 hover:text-red-400 hover:bg-gray-200 p-2 block mt-2 text-sm w-full text-left">Delete User</button>
                     </div>
                     <div class="">
                         <button class="p-3 bg-[#6D5BD0] text-white font-bold rounded-lg">PAY DUES</button>
@@ -38,106 +44,13 @@
                 </div>
                 <div class="" v-if="searchtext.length == 0">
                     <div v-for="(userdata, index) in usingsliceddata" :key="index" class="">
-                        <div v-if="showing == 'All' || userdata.payment_status == showing" class="flex flex-nowrap p-4 pl-11 pt-3 overflow-x-auto border-b border-[#D9D5EC]" :class="{'bg-[#F2F0F9]' : currentitemid == userdata.trx_id && currentlychecked == true }">
-                            <div class="w-1/3 overflow-x-auto text-xs"><input type="checkbox" @change="currentlyOn(userdata.trx_id, $event)" class="p-2 w-5 h-5 rounded-md inline-block"/> <button @click="showingdownid = userdata.trx_id; showingdown = !showingdown" class="inline-block ml-4 mt-3"><img v-if="showingdown == true && showingdownid == userdata.trx_id" src="~/assets/Up.png"  class="-mt-3 ml-4 h-5 w-5 inline-block"><img v-if="showingdown == false || showingdownid != userdata.trx_id" src="~/assets/down-arrow.png"  class="-mt-3 ml-4 h-5 w-5 inline-block"></button></div>
-                            <div class="w-1/2 overflow-x-auto text-sm">{{ userdata.first_name }} {{ userdata.last_name }} <br> <span class="text-gray-400 text-xs">{{ userdata.email }}</span></div>
-                            <div class="w-9/12 overflow-x-auto text-xs">
-                                <span class="rounded-2xl px-2" :class="{ 'bg-[#E6E6F2] text-[#4A4AFF]' : userdata.user_status == 'Active', 'text-[#6E6893] bg-[#F2F0F9]' : userdata.user_status == 'Inactive'}"><span :class="{ 'bg-[#4A4AFF]' : userdata.user_status == 'Active', 'bg-[#6E6893]' : userdata.user_status == 'Inactive'}" class="w-2 h-2 mr-1 inline-block rounded-full"></span>{{ userdata.user_status }}</span>
-                                <p class="py-2 text-gray-400">Last login : {{ userdata.last_seen }}</p>
-                            </div>
-                            <div class="w-5/12 overflow-x-auto text-xs">
-                                <span class="rounded-2xl px-2" :class="{ 'bg-[#CDFFCD] text-[#007F00]' : userdata.payment_status == 'Paid', 'text-[#CE8500] bg-[#FFECCC]' : userdata.payment_status == 'Unpaid', 'text-[#D30000] bg-[#FFE0E0]' : userdata.payment_status == 'Overdue'}"><span :class="{ 'bg-[#007F00]' : userdata.payment_status == 'Paid', 'bg-[#CE8500]' : userdata.payment_status == 'Unpaid', 'bg-[#D30000]' : userdata.payment_status == 'Overdue'}" class="w-2 h-2 mr-1 inline-block rounded-full"></span>{{ userdata.payment_status }}</span>
-                                <p class="py-2 text-gray-400">{{ userdata.payment_status == 'Paid' ? 'Paid on' : userdata.payment_status == 'Unpaid' ? 'Dues on' : userdata.payment_status == 'Overdue' ? 'Dued on' : '' }} {{ userdata.payment_status == 'Paid' ? userdata.payment_date : userdata.due_date}}</p>
-                            </div>
-                            <div class="w-1/4 overflow-x-auto text-md">{{ userdata.symbol }}{{ userdata.amount }} <br><span class="block pl-2 text-sm text-gray-400">{{ userdata.currency }}</span></div>
-                            <!-- <div class="w-1/3 overflow-x-auto text-xs">view more</div> -->
-                            <div class="w-1/5 overflow-x-auto text-xs text-right">
-                                <button><span class="inline-block mr-3 text-[#6E6893]">View more</span> <img src="~/assets/More.png" class="inline-block"></button>
-                            
-                            </div>
-                            
-                        </div>
-                        <div class="absolute hidden h-contain m-2 p-2 z-5 w-40 right-2 bg-gray-200">
-                            
-                            <div class="absolute -top-4 right-2 h-fit bg-white"><img src="~/assets/ellipse.png"  class=""> <span class="relative -right-2 -top-6">x</span></div>
-                            <NuxtLink class="block">Edit</NuxtLink>
-                            <NuxtLink class="block">View Profile</NuxtLink>
-                            <NuxtLink class="block text-[#007F00]">Activate User</NuxtLink>
-                            <NuxtLink class="block text-[#D30000] mt-2">Delete</NuxtLink>
-                        </div>
-
-                        <div class="bg-[#F4F2FF]" v-if="showingdownid == userdata.trx_id && showingdown == true">
-                            <div class="flex flex-nowrap bg-[#F2F0F9] p-4 pl-40 border-y-1 border-[#D9D5EC] overflow-x-auto">
-                                <div class="w-1/4 overflow-x-auto text-xs">DATE</div>
-                                <div class="w-1/4 overflow-x-auto text-xs">USER ACTIVITY</div>
-                                <div class="w-1/4 overflow-x-auto text-xs">DETAILS <img src="~/assets/details.png" class="inline-block ml-3 -mt-1"></div>
-                            </div>
-                            <div v-if="userdata.user_activity.length > 0">
-                                <div v-for="(activity, index) in userdata.user_activity" :key="index" class="">
-                                    <div class="flex flex-nowrap p-4 pl-40 mt-3 overflow-x-auto border-b border-[#D9D5EC]">
-                                        <div class="w-1/4 overflow-x-auto text-sm">{{ activity.date }}</div>
-                                        <div class="w-1/4 overflow-x-auto text-xs">
-                                            {{ activity.activity }}
-                                        </div>
-                                        <div class="w-1/4 overflow-x-auto text-xs">
-                                            {{ activity.details }}
-                                        </div>
-                                    </div>
-
-                                </div>
-                            </div>
-                            <div v-else>
-                                <p class="text-xl text-center p-5 text-[#6E6893]">NO RECORDS FOUND</p>
-                            </div>
-                        </div>
+                        <BaseList v-if="showing == 'All' || userdata.payment_status == showing" :checkall="checkall" :userdata="userdata"/>
                         
                     </div>                
                 </div>
                 <div v-else>
                     <div v-for="(userdata, index) in usingsliceddata" :key="index" class="">
-                        <div v-if="showing == 'All' || userdata.payment_status == showing" class="flex flex-nowrap p-4 pl-11 mt-3 overflow-x-auto border-b border-[#D9D5EC]">
-                            <div class="w-1/3 overflow-x-auto text-xs"><input type="checkbox" v-model="checkall" class="p-2 w-5 h-5 rounded-md inline-block"/> <button @click="showingdownid = userdata.trx_id; showingdown = !showingdown" class="inline-block ml-4 mt-3"><img src="~/assets/down-arrow.png"  class="-mt-3 ml-4 h-5 w-5 inline-block"></button></div>
-                            <div class="w-1/2 overflow-x-auto text-sm">{{ userdata.first_name }} {{ userdata.last_name }} <br> <span class="text-gray-400 text-xs">{{ userdata.email }}</span></div>
-                            <div class="w-9/12 overflow-x-auto text-xs">
-                                <span class="rounded-2xl px-2" :class="{ 'bg-[#E6E6F2] text-[#4A4AFF]' : userdata.user_status == 'Active', 'text-[#6E6893] bg-[#F2F0F9]' : userdata.user_status == 'Inactive'}"><span :class="{ 'bg-[#4A4AFF]' : userdata.user_status == 'Active', 'bg-[#6E6893]' : userdata.user_status == 'Inactive'}" class="w-2 h-2 mr-1 inline-block rounded-full"></span>{{ userdata.user_status }}</span>
-                                <p class="py-2 text-gray-400">Last login : {{ userdata.last_seen }}</p>
-                            </div>
-                            <div class="w-5/12 overflow-x-auto text-xs">
-                                <span class="rounded-2xl px-2" :class="{ 'bg-[#CDFFCD] text-[#007F00]' : userdata.payment_status == 'Paid', 'text-[#CE8500] bg-[#FFECCC]' : userdata.payment_status == 'Unpaid', 'text-[#D30000] bg-[#FFE0E0]' : userdata.payment_status == 'Overdue'}"><span :class="{ 'bg-[#007F00]' : userdata.payment_status == 'Paid', 'bg-[#CE8500]' : userdata.payment_status == 'Unpaid', 'bg-[#D30000]' : userdata.payment_status == 'Overdue'}" class="w-2 h-2 mr-1 inline-block rounded-full"></span>{{ userdata.payment_status }}</span>
-                                <p class="py-2 text-gray-400">{{ userdata.payment_status == 'Paid' ? 'Paid on' : userdata.payment_status == 'Unpaid' ? 'Dues on' : userdata.payment_status == 'Overdue' ? 'Dued on' : '' }} {{ userdata.payment_status == 'Paid' ? userdata.payment_date : userdata.due_date}}</p>
-                            </div>
-                            <div class="w-1/4 overflow-x-auto text-md">{{ userdata.symbol }}{{ userdata.amount }} <br><span class="block pl-2 text-sm text-gray-400">{{ userdata.currency }}</span></div>
-                            <!-- <div class="w-1/3 overflow-x-auto text-xs">view more</div> -->
-                            <div class="w-1/5 overflow-x-auto text-xs text-right">
-                                <button><span class="inline-block mr-3 text-[#6E6893]">View more</span> <img src="~/assets/More.png" class="inline-block"></button>
-                            </div>
-                        </div>
-
-                        <div class="bg-[#F4F2FF]" v-if="showingdownid == userdata.trx_id && showingdown == true">
-                            <div class="flex flex-nowrap bg-[#F2F0F9] p-4 pl-40 border-y-1 border-[#D9D5EC] overflow-x-auto">
-                                <div class="w-1/4 overflow-x-auto text-xs">DATE</div>
-                                <div class="w-1/4 overflow-x-auto text-xs">USER ACTIVITY</div>
-                                <div class="w-1/4 overflow-x-auto text-xs">DETAILS <img src="~/assets/details.png" class="inline-block ml-3 -mt-1"></div>
-                            </div>
-                            <div v-if="userdata.user_activity.length > 0">
-                                <div v-for="(activity, index) in userdata.user_activity" :key="index" class="">
-                                    <div class="flex flex-nowrap p-4 pl-40 mt-3 overflow-x-auto border-b border-[#D9D5EC]">
-                                        <div class="w-1/4 overflow-x-auto text-sm">{{ activity.date }}</div>
-                                        <div class="w-1/4 overflow-x-auto text-xs">
-                                            {{ activity.activity }}
-                                        </div>
-                                        <div class="w-1/4 overflow-x-auto text-xs">
-                                            {{ activity.details }}
-                                        </div>
-                                    </div>
-
-                                </div>
-                            </div>
-                            <div v-else>
-                                <p class="text-xl text-center p-5 text-[#6E6893]">NO RECORDS FOUND</p>
-                            </div>
-                        </div>
-                        
+                        <BaseList v-if="showing == 'All' || userdata.payment_status == showing" :checkall="checkall" :userdata="userdata"/>
                     </div>
                 </div>
 
@@ -563,6 +476,8 @@ let intervalnum = ref(10)
 let endnum = ref(10)
 const endss = computed(() => { return usingusersdata.value.length })
 let limitnum = endss
+let showactions = ref(false)
+let showfilters = ref(false)
 
 
 // usingusersdata.value = usersdata;
